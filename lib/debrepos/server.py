975543aabda0149fce8c71617086010d74ab9f07
(no author) <(no author)@b9a5c164-3831-4c0c-aefd-67cc14e9e19c>
2012-07-06 20:03:22 +0000

    added code to help manage repos

---
+++ lib/debrepos/server.py
@@ -0,0 +1,55 @@
+import SimpleXMLRPCServer
+
+from useless.base.path import path
+
+from debrepos.filterlist import selections_to_dictionary
+
+
+class TestService(object):
+    def __init__(self):
+        self.foo = 'bar'
+
+    def upper(self, astr):
+        return astr.upper()
+
+    def lower(self, astr):
+        return astr.lower()
+    
+
+class PartialMirrorManager(object):
+    def __init__(self):
+        self.baseparent = path('/freespace/debrepos')
+
+    def main_list_filename(self, dist):
+        basename = '%s.pkgs' % dist
+        directory = self.baseparent / 'debian/conf/pkgs'
+        filename = directory / basename
+        return filename
+
+    def get_selections(self, dist):
+        filename = self.main_list_filename(dist)
+        return selections_to_dictionary(filename, install=True)
+    
+        
+
+    
+def make_server(host, port, instance):
+    server = SimpleXMLRPCServer.SimpleXMLRPCServer((host, port))
+    server.register_instance(instance)
+    return server
+
+
+
+if __name__ == '__main__':
+    import sys
+    if sys.argv[1] == 'client':
+        import xmlrpclib
+        remote = xmlrpclib.Server('http://localhost:8000')
+        r = remote
+    else:
+        srv = PartialMirrorManager()
+        server = make_server('localhost', 8000, srv)
+        server.serve_forever()
+        
+
+    
